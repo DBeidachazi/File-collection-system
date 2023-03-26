538ee24d93299564ab7d0d858abd98cbf58ea9a4
DBeidachazi <269502169@qq.com>
2023-03-26 21:20:12 +0800

    修改班级搜索response

--- api/class_api/class_search.go
+++ api/class_api/class_search.go
@@ -3,6 +3,7 @@ package class_api
 import (
 	"FengfengStudy/global"
 	"FengfengStudy/models/orm/dal"
+	"FengfengStudy/models/orm/model"
 	"FengfengStudy/models/res"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
@@ -10,6 +11,10 @@ import (
 
 type ClassSearchRequest struct {
 	StuId int32 `json:"stu_id" bind:"required"`
+}
+type ClassResponse struct {
+	ClassUser *model.ClassUser `json:"class_user"`
+	ClassName string           `json:"class_name"`
 }
 
 func (ClassApi) SearchClassView(c *gin.Context) {
@@ -26,7 +31,21 @@ func (ClassApi) SearchClassView(c *gin.Context) {
 		res.FailWithMessage("没有创建班级", c)
 		return
 	}
-	res.Ok(findClassUser, "查询班级成功", c)
+	var classResponse []ClassResponse
+	for _, v := range findClassUser {
+		findClass, err := dal.Class.Where(dal.Class.ClassID.Eq(v.ClassID)).First()
+		if err != nil {
+			global.Log.Warnln("查询班级失败: ", v.ClassID)
+			logrus.Warnln("err: ", err)
+			res.FailWithMessage("查询班级失败", c)
+			return
+		}
+		classResponse = append(classResponse, ClassResponse{
+			ClassUser: v,
+			ClassName: findClass.ClassName,
+		})
+	}
+	res.Ok(classResponse, "查询班级成功", c)
 	/* todo 班级查询
 	1. 查询班级用户表，获取全部班级id
 	2. 管理权限 0:普通用户 1:管理员 2: 班级创建者 ,
